cae7013590b03123be97c7d83b12495e249debc9
vivek80801 <57554760+vivek80801@users.noreply.github.com>
2025-12-31 12:34:28 +0530

    [Format]: fixed fromatting

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -4,7 +4,7 @@ namespace Tests\Feature;
 
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-#use Illuminate\Foundation\Testing\WithFaker;
+// use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 
@@ -13,61 +13,62 @@ class AuthTest extends TestCase
     use RefreshDatabase;
 
     private $user;
+
     protected function setUp(): void
     {
         parent::setUp();
         $this->user = [
-            "name" => "user",
-            "email" => "user@email.com",
-            "password" => "password",
+            'name' => 'user',
+            'email' => 'user@email.com',
+            'password' => 'password',
         ];
 
         User::create(
             [
-                "name" => $this->user["name"],
-                "email" => $this->user["email"],
-                "password" => Hash::make($this->user["password"]),
+                'name' => $this->user['name'],
+                'email' => $this->user['email'],
+                'password' => Hash::make($this->user['password']),
             ]
         );
     }
 
-    public function test_login (): void
+    public function test_login(): void
     {
-        $response = $this->post("/login", [
-            "email" => $this->user["email"],
-            "password" => $this->user["password"],
+        $response = $this->post('/login', [
+            'email' => $this->user['email'],
+            'password' => $this->user['password'],
         ]);
 
         $response->assertSessionHasNoErrors();
         $response->assertStatus(302);
-        $response->assertRedirect("dashboard");
+        $response->assertRedirect('dashboard');
     }
 
-    public function test_login_validation_when_email_is_empty():void
+    public function test_login_validation_when_email_is_empty(): void
     {
-        $response = $this->post("/login", [
-            "email" => "",
-            "password" => $this->user["password"],
+        $response = $this->post('/login', [
+            'email' => '',
+            'password' => $this->user['password'],
         ]);
 
         $response->assertSessionHasErrors('email');
     }
 
-    public function test_login_validation_when_password_is_empty():void
+    public function test_login_validation_when_password_is_empty(): void
     {
-        $response = $this->post("/login", [
-            "email" => $this->user["email"],
-            "password" => "",
+        $response = $this->post('/login', [
+            'email' => $this->user['email'],
+            'password' => '',
         ]);
 
         $response->assertSessionHasErrors('password');
     }
 
-    public function test_login_validation_when_email_and_password_is_empty():void
+    public function test_login_validation_when_email_and_password_is_empty(): void
     {
-        $response = $this->post("/login", [
-            "email" => "",
-            "password" => "",
+        $response = $this->post('/login', [
+            'email' => '',
+            'password' => '',
         ]);
 
         $response->assertSessionHasErrors(['password', 'email']);
