6336ae02f2de79c1d95f9becac6e130776b3ca87
vivek80801 <57554760+vivek80801@users.noreply.github.com>
2026-02-18 10:17:18 +0530

    [Fix]: Role issue was giving unauthroise error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,14 @@
 
 namespace App\Providers;
 
+use App\Policies\RolePolicy;
 use App\Repositories\Interfaces\UserRepositoryInterface;
 use App\Repositories\UserRepository;
 use App\Utilities\MenuBuilder;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
+use Spatie\Permission\Models\Role;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,6 +29,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Gate::policy(Role::class, RolePolicy::class);
         //
     }
 }
