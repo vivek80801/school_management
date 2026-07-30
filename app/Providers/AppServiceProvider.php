83e4ffb8c2be0dabb3e9eb9e56164ffa30bbe3c9
vivek80801 <57554760+vivek80801@users.noreply.github.com>
2026-07-30 13:39:58 +0530

    [Feature]: Rate limiter added

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,7 +6,10 @@ use App\Policies\RolePolicy;
 use App\Repositories\Interfaces\UserRepositoryInterface;
 use App\Repositories\UserRepository;
 use App\Utilities\MenuBuilder;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Spatie\Permission\Models\Role;
@@ -29,10 +32,19 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+
+        RateLimiter::for('login', function (Request $request) {
+            return [
+                Limit::perMinute(5)->by($request->ip()),
+                Limit::perMinute(3)->by(
+                    $request->ip().'|'.$request->input('email')
+                ),
+            ];
+        });
+
         if (app()->environment('production')) {
             URL::forceScheme('https');
         }
         Gate::policy(Role::class, RolePolicy::class);
-        //
     }
 }
